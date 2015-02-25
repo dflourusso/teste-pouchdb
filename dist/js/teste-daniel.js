f50a87e213cb47bee104af34079041e7b57bf6b0
Daniel Fernando Lourusso <dflourusso@gmail.com>
2015-02-25 17:05:08 -0300

    funcionamento basico do todo

--- dist/js/teste-daniel.js
+++ dist/js/teste-daniel.js
@@ -1,9 +1,9 @@
 (function() {
-  var Todo, testeDaniel;
+  var testeDaniel;
 
   testeDaniel = (function() {
     function testeDaniel() {
-      return ['pouchdb'];
+      return [];
     }
 
     return testeDaniel;
@@ -11,28 +11,5 @@
   })();
 
   angular.module('teste-daniel', new testeDaniel());
-
-  Todo = (function() {
-    function Todo(pouchDB) {
-      this.pouchDB = pouchDB;
-      this.remoteCouch = "http://192.168.25.8:5984/todos";
-      this.db = this.pouchDB(this.remoteCouch);
-      this.sync();
-    }
-
-    Todo.prototype.sync = function() {
-      var options;
-      options = {
-        live: true
-      };
-      this.db.replicate.to(this.remoteCouch, options);
-      return this.db.replicate.from(this.remoteCouch, options);
-    };
-
-    return Todo;
-
-  })();
-
-  angular.module('teste-daniel').service('todoService', ['pouchDB', Todo]);
 
 }).call(this);
