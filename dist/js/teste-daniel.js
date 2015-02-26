a8f950e2d931cb6748222b2cdbdd79d3e107f968
Daniel Fernando Lourusso <dflourusso@gmail.com>
2015-02-26 09:19:03 -0300

    tentativa de resolver conflitos

--- dist/js/teste-daniel.js
+++ dist/js/teste-daniel.js
@@ -1,5 +1,6 @@
 (function() {
-  var Todo, TodoModel, testeDaniel;
+  var Todo, TodoModel, testeDaniel,
+    indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
 
   testeDaniel = (function() {
     function testeDaniel() {
@@ -13,6 +14,8 @@
   angular.module('teste-daniel', new testeDaniel());
 
   Todo = (function() {
+    var onChange;
+
     Todo.prototype.todos = {};
 
     function Todo(pouchDB, TodoModel1) {
@@ -20,6 +23,7 @@
       this.remoteCouch = "http://192.168.25.8:5984/todos";
       this.db = pouchDB(this.remoteCouch);
       this.sync();
+      this.changes();
     }
 
     Todo.prototype.sync = function() {
@@ -53,6 +57,40 @@
       })(this));
     };
 
+    onChange = function(doc) {
+      console.log(doc._conflicts);
+      if (!doc._conflicts) {
+        return;
+      }
+      collectConflicts(doc._conflicts, function(docs) {
+        var master, prop;
+        master = docs.sort(byTime).unshift();
+        for (doc in docs) {
+          for (prop in doc) {
+            if (!(indexOf.call(master, prop) >= 0)) {
+              master[prop] = doc[prop];
+            }
+          }
+        }
+        this.db.put(master, (function(_this) {
+          return function(err) {
+            if (!err) {
+              for (doc in docs) {
+                _this.db.remove(doc);
+              }
+            }
+          };
+        })(this));
+      });
+    };
+
+    Todo.prototype.changes = function() {
+      return this.db.changes({
+        conflicts: true,
+        onChange: onChange
+      });
+    };
+
     Todo.prototype.insert = function(todo) {
       var p;
       p = this.db.post(todo);
@@ -68,17 +106,17 @@
       })(this));
     };
 
-    Todo.prototype.removeTodo = function(todo) {
-      return this.db.remove(todo);
-    };
-
     Todo.prototype.update = function(doc) {
       this.doc = doc ? doc : this.doc;
-      return this.db.put(doc)["catch"](function(err) {
+      return this.db.put(this.doc)["catch"](function(err) {
         if (err.status !== 409) {
           return this.update();
         }
       });
+    };
+
+    Todo.prototype.removeTodo = function(todo) {
+      return this.db.remove(todo);
     };
 
     return Todo;
--- src/services/Todo.coffee
+++ src/services/Todo.coffee
@@ -4,6 +4,7 @@ class Todo extends Service
     @remoteCouch = "http://192.168.25.8:5984/todos"
     @db = pouchDB @remoteCouch
     @sync()
+    @changes()
 
   sync: ->
     @updateTodos()
@@ -16,6 +17,31 @@ class Todo extends Service
     @db.allDocs(options).then (doc)=>
       @todos = doc.rows
 
+  onChange = (doc) ->
+    console.log doc._conflicts
+    unless doc._conflicts
+      return
+    collectConflicts doc._conflicts, (docs) ->
+      master = docs.sort(byTime).unshift()
+      for doc of docs
+        for prop of doc
+          if !(prop in master)
+            master[prop] = doc[prop]
+      @db.put master, (err)=>
+        if !err
+          for doc of docs
+            @db.remove doc
+        return
+      return
+    return
+
+  changes: ->
+    @db.changes
+      conflicts: true
+      onChange: onChange
+
+
+
 
 
 
@@ -26,11 +52,11 @@ class Todo extends Service
     p.catch (e)=>
       console.log 'Erro ao salvar doc', e
 
-  removeTodo: (todo)->
-    @db.remove todo
-
   update: (doc)->
     @doc = if doc then doc else @doc
-    @db.put(doc).catch (err)->
+    @db.put(@doc).catch (err)->
       if err.status != 409
         @update()
+
+  removeTodo: (todo)->
+    @db.remove todo
