150832bf3bdd79a2834362535c2388690cdce0c0
Daniel Fernando Lourusso <dflourusso@gmail.com>
2015-02-27 14:47:06 -0300

    add attachments

--- examples/app.coffee
+++ examples/app.coffee
@@ -1,9 +1,10 @@
-angular.module('app', ['teste-daniel'])
+angular.module('app', ['teste-daniel', 'file-model'])
 
-.controller 'ctrl', ['$scope', '$window', 'pouchDB', 'TodoModel', ($scope, $window, pouchDB, TodoModel)->
+.controller 'ctrl', ['$q', '$scope', '$window', 'pouchDB', ($q, $scope, $window, pouchDB)->
   remote = "http://192.168.25.8:5984/todos"
   local = 'todos'
   $scope.docs = []
+  $scope.attachment = null
 
   db = pouchDB local
   $scope.sync = ->
@@ -12,17 +13,6 @@ angular.module('app', ['teste-daniel'])
 
   window.addEventListener 'online', ->
     $scope.sync()
-
-
-  $scope.add = ->
-    db.post date: new Date().toJSON()
-
-  $scope.remove = (doc)->
-    db.get(doc._id).then((_doc)=>
-      return db.remove(_doc);
-    ).catch (err)=>
-      console.log 'Erro ao remover'
-
 
   onChange = =>
     options =
@@ -35,9 +25,26 @@ angular.module('app', ['teste-daniel'])
     live: true,
     onChange: onChange
 
-  console.log db.changes(options)
   db.changes(options).then(null, null, onChange)
 
+
+
+  $scope.add = ->
+    db.post date: new Date().toJSON()
+
+  $scope.addAttach = (attachment, doc)->
+    return unless attachment
+    attachment_id = new Date().toJSON()
+    db.putAttachment doc._id, attachment_id, doc._rev, attachment, attachment.type, (err, res)->
+      console.log err, res
+
+
+
+  $scope.remove = (doc)->
+    db.get(doc._id).then((_doc)=>
+      return db.remove(_doc);
+    ).catch (err)=>
+      console.log 'Erro ao remover'
 
 ]
 ready = if navigator.userAgent.indexOf('(cordova;') > -1 then 'deviceready' else 'DOMContentLoaded'
--- examples/app.js
+++ examples/app.js
@@ -1,12 +1,13 @@
 (function() {
   var init, ready;
 
-  angular.module('app', ['teste-daniel']).controller('ctrl', [
-    '$scope', '$window', 'pouchDB', 'TodoModel', function($scope, $window, pouchDB, TodoModel) {
+  angular.module('app', ['teste-daniel', 'file-model']).controller('ctrl', [
+    '$q', '$scope', '$window', 'pouchDB', function($q, $scope, $window, pouchDB) {
       var db, local, onChange, options, remote;
       remote = "http://192.168.25.8:5984/todos";
       local = 'todos';
       $scope.docs = [];
+      $scope.attachment = null;
       db = pouchDB(local);
       $scope.sync = function() {
         return db.sync(remote, {
@@ -17,22 +18,6 @@
       window.addEventListener('online', function() {
         return $scope.sync();
       });
-      $scope.add = function() {
-        return db.post({
-          date: new Date().toJSON()
-        });
-      };
-      $scope.remove = function(doc) {
-        return db.get(doc._id).then((function(_this) {
-          return function(_doc) {
-            return db.remove(_doc);
-          };
-        })(this))["catch"]((function(_this) {
-          return function(err) {
-            return console.log('Erro ao remover');
-          };
-        })(this));
-      };
       onChange = (function(_this) {
         return function() {
           var options;
@@ -49,8 +34,33 @@
         live: true,
         onChange: onChange
       };
-      console.log(db.changes(options));
-      return db.changes(options).then(null, null, onChange);
+      db.changes(options).then(null, null, onChange);
+      $scope.add = function() {
+        return db.post({
+          date: new Date().toJSON()
+        });
+      };
+      $scope.addAttach = function(attachment, doc) {
+        var attachment_id;
+        if (!attachment) {
+          return;
+        }
+        attachment_id = new Date().toJSON();
+        return db.putAttachment(doc._id, attachment_id, doc._rev, attachment, attachment.type, function(err, res) {
+          return console.log(err, res);
+        });
+      };
+      return $scope.remove = function(doc) {
+        return db.get(doc._id).then((function(_this) {
+          return function(_doc) {
+            return db.remove(_doc);
+          };
+        })(this))["catch"]((function(_this) {
+          return function(err) {
+            return console.log('Erro ao remover');
+          };
+        })(this));
+      };
     }
   ]);
 
